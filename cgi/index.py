689502cb05176f7b7fb6a5a601c685c6cab9bf39
nikitaGrynch <nikita.grynch@gmail.com>
2024-01-10 21:40:45 +0200

    show_databases to html

--- cgi/index.py
+++ cgi/index.py
@@ -1,6 +1,11 @@
 #!/usr/local/bin/python3
 
 import os
+import mysql.connector
+import sys
+sys.path.append( '../' )
+import db_ini
+
 
 # сформувати з змінних перелік <ul> name = value
 # envs = os.environ  # змінні оточення -- dict{ name: value }
@@ -10,18 +15,39 @@ import os
 #                        else "" 
 #                        for k,v in os.environ.items()])}</ul>"
 
-envs = {k: v for k, v in os.environ.items() if k in ["REQUEST_URI", "QUERY_STRING", "REQUEST_METHOD", "REMOTE_ADDR", "REQUEST_SCHEME"]}
-envs_html = f"<ul>{ "".join([f"<li>{k} = {v}</li>" for k,v in envs.items()]) }"
-envs_html += "</ul>"
-if envs["QUERY_STRING"]:
-    envs_html += f"<p>QUERY_STRING: {f"<ul>{ "".join([f"<li>{k} = {v}</li>" for k,v in {k: v for k, v in (pair.split('=') for pair in envs["QUERY_STRING"].split('&'))}.items()]) }"}</p>"
+# envs = {k: v for k, v in os.environ.items() if k in ["REQUEST_URI", "QUERY_STRING", "REQUEST_METHOD", "REMOTE_ADDR", "REQUEST_SCHEME"]}
+# envs_html = f"<ul>{ "".join([f"<li>{k} = {v}</li>" for k,v in envs.items()]) }"
+# envs_html += "</ul>"
+# if envs["QUERY_STRING"]:
+#     envs_html += f"<p>QUERY_STRING: {f"<ul>{ "".join([f"<li>{k} = {v}</li>" for k,v in {k: v for k, v in (pair.split('=') for pair in envs["QUERY_STRING"].split('&'))}.items()]) }"}</p>"
 
-print( "Content-Type: text/html; charset=cp1251" )
-print( "Connection: close" )
-print()   # порожній рядок - кінець заголовків
+# print( "Content-Type: text/html; charset=cp1251" )
+# print( "Connection: close" )
+# print()   # порожній рядок - кінець заголовків
 # print(envs_html)
-with open( 'home.html' ) as file :
-    print( file.read() )
+# with open( 'home.html' ) as file :
+#     print( file.read() )
+
+
+#HW SHOW_DATABASES to HTML
+db = mysql.connector.connect( **db_ini.connection_params )
+sql = "SHOW DATABASES"
+try :
+    with db.cursor() as cursor :
+        cursor.execute( sql )
+        html_table = "<table border='1'><tr><th>Database Name</th></tr>"
+        for row in cursor:
+            html_table += "<tr>"
+            html_table += f"<td>{row[0]}</td>"
+            html_table += "</tr>"
+        html_table += "</table>"
+except mysql.connector.Error as err :
+    print( err )
+else :
+    print( "Content-Type: text/html; charset=cp1251" )
+    print( "Connection: close" )
+    print()
+    print(html_table)
 
 
 """
